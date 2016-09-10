414cc6239c8917f886e600e069257266ee3d2623
byungjun Lee <byungjun Lee>
2016-09-10 22:22:30 +0900

    ' 0910 KMU BigData Lectures Modify and Add Class

--- KMU/Second Semester/DataMining/0910/demo.R
+++ KMU/Second Semester/DataMining/0910/demo.R
@@ -1,11 +1,12 @@
 # Data Preparation
 
-setwd("d:/datamining")
+setwd("d:/DataScience/KMU/Second Semester/DataMining/0910/")
 
 train <- read.csv("pepTrainSet.csv", stringsAsFactors=F)
 train <- subset(train, select=-c(id))
 test <- read.csv("pepTestSet.csv", stringsAsFactors=F)
 newd <- read.csv("pepNewCustomers.csv", stringsAsFactors=F)
+str(train)
 
 train$pep <- factor(train$pep)
 test$pep <- factor(test$pep)
@@ -13,14 +14,17 @@ test$pep <- factor(test$pep)
 
 # Modeling
 
-install.packages("caret")
-install.packages("ROCR")
-install.packages("C50")
+#install.packages("caret",repos = "http://cran.us.r-project.org") #데이너 전처리 / 모델
+#install.packages("ROCR",repos = "http://cran.us.r-project.org") # 모형의 그래프 생성과 평가
+#install.packages("C50",repos = "http://cran.us.r-project.org") # 분류 분석, 의사결정나무 (Decision Tree)
 
 library(caret)
 library(ROCR)
 library(C50)
 
+?C5.0Control
+
+# Various parameters that control aspects of the C5.0 fit.
 c5_options <- C5.0Control(winnow = FALSE, noGlobalPruning = FALSE)
 c5_model <- C5.0(pep ~ ., data=train, control=c5_options, rules=FALSE)
 summary(c5_model)
@@ -31,10 +35,15 @@ summary(lm_model)
 
 
 # Evaluation
-
+install.packages("e1071")
+library(e1071)
 test$c5_pred <- predict(c5_model, test, type="class")
 test$c5_pred_prob <- predict(c5_model, test, type="prob")
-confusionMatrix(test$c5_pred, test$pep)
+confusionMatrix(test$c5_pred, test$pep) # .No, .Yes생성하는
+
+head(test)
+#pred.prob.No -> NO 의 확률 
+#pred.prob.Yes -> Yes의 확률 
 
 test$lm_pred <- ifelse(predict(lm_model, test, type="response") > 0.5, "YES", "NO")
 test$lm_pred_prob <- predict(lm_model, test, type="response")
