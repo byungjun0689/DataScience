0edd9eb613961c36b3bfe653342cbb35a10d1a74
byungjun <byungjun0689@gmail.com>
2016-12-05 08:40:45 +0900

    add Utilites

---
+++ Python/Utilites/findStringinVBForm/findStringinVB.py
@@ -0,0 +1,55 @@
+import os
+import time
+import sys,getopt
+
+
+def search(dirname,findstr):
+    
+    dirlist = os.listdir(dirname)
+    for i in dirlist:  
+        spath = os.path.join(dirname,i)
+        if os.path.isfile(spath):
+            ext = os.path.splitext(spath)[-1]
+            if ext == ".frm":
+                try:
+                    #print(path)
+                    if findstr in open(spath,'r').read():
+                        print(spath)#print(findstr+" 문자열이 "+path+" 에 존재.")
+						
+                except:
+                    print("에러 : "+ spath)
+                    #f findstr in open(path,'r','utf-8').read():
+                        #rint(findstr+" 문자열이 "+path+" 에 존재.")
+        elif os.path.isdir(spath):
+            search(spath,findstr)
+        else:
+            print("오잉")
+
+
+def search2(dirname,findstr):
+    
+    dirlist = os.listdir(dirname)
+    for i in dirlist:  
+        spath = os.path.join(dirname,i)
+        if os.path.isfile(spath):
+            ext = os.path.splitext(spath)[-1]
+            if ext == ".frm":
+                try:
+                    with open(spath,'r') as f:
+                        for i,l in enumerate(f):
+                            if findstr in l:
+                                print(i+" 열 " + path + "에 존재")
+                except:
+                    print("에러 : "+ spath)
+        elif os.path.isdir(spath):
+            search2(spath,findstr)
+        else:
+            print("오잉")
+
+
+if __name__ == "__main__":
+	print(sys.argv[:])
+	if sys.argv[1] == "help":
+		print("인자 1: 시작 폴더  인자 2: 찾고싶은 문자열 현재는 frm파일만 검색가능.")
+	print("검색시장 15초 정도의 시간소요")
+	search(sys.argv[1],sys.argv[2])
