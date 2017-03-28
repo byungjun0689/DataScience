1cd07446af7c1775d8052339920d47e473e5c31e
byungjun <byungjun0689@gmail.com>
2017-03-28 13:31:54 +0900

    Finish Uber & Start Alcohol Student

--- 8. Kaggle/4. MyUberDrives/Uber.py
+++ 8. Kaggle/4. MyUberDrives/Uber.py
@@ -10,10 +10,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns 
 import datetime as dt
+import networkx
+from collections import Counter
 
 uber = pd.read_csv("Uber.csv")
 uber.columns = ['START_DATE','END_DATE','CATEGORY','START','STOP','MILES','PURPOSE']
-pd.to_datetime(uber['START_DATE'], format='%m/%d/%Y %H:%M')
 
 uber = uber.drop(uber.index[len(uber)-1])
 
@@ -43,6 +44,14 @@ cnt_start.head()
 sns.barplot(x='START', y='cnt', data=cnt_start.head(10))
 plt.xticks(rotation=45)
 
+cnt_stop = pd.DataFrame({'cnt' : uber.groupby(['STOP']).size()}).reset_index()
+cnt_stop = cnt_stop.sort_values(by='cnt',ascending=False)
+cnt_stop.head()
+
+sns.barplot(x='STOP', y='cnt', data=cnt_stop.head(10))
+plt.xticks(rotation=45)
+
+
 sns.boxplot(x='PURPOSE',y='MILES',data=uber[uber['MILES']<60],hue='CATEGORY')
 plt.xticks(rotation=45)
 
@@ -62,7 +71,42 @@ pairs_SE['location'] = attachDestination(pairs_SE)
 sns.barplot(x='location', y='cnt', data=pairs_SE[pairs_SE['location']!='Unknown Location-Unknown Location'].head(10))
 plt.xticks(rotation=45)
 
-date_uber = pd.DataFrame({'cnt' : uber.groupby(['SDATE']).size()}).reset_index()
-date_uber
+pairs_SE = pairs_SE[pairs_SE['cnt'] < 10]
+
+pairs_zip = list(zip(pairs_SE.ix[:,0],pairs_SE.ix[:,1],pairs_SE.ix[:,2]))
+
+edges = []
+for i in range(len(pairs_zip)):
+    edges.append((pairs_zip[i][0],pairs_zip[i][1], pairs_zip[i][2]))
+
+edge_list = [(word1, word2) for word1, word2, weight in edges]
+weight_list = [weight for word1, word2, weight in edges]
+
+
+G = networkx.Graph()
+edge_set = set()
+for word1, word2, weight in edges:
+    G.add_edge(word1, word2, weight=weight)
+    edge_set.add((word1,word2))
+    
+position = networkx.spring_layout(G, k=0.09,  iterations=100)
+plt.figure(figsize=(20, 15))
+networkx.draw_networkx_nodes(G, position, node_size=0)
+networkx.draw_networkx_edges(G, position, edgelist=edge_list, width=weight_list, edge_color='lightgray')
+networkx.draw_networkx_labels(G, position, font_size=15)
+plt.axis('off')
+plt.show()
+
+#1. date_uber = pd.DataFrame({'cnt' : uber.groupby(['SDATE']).size()}).reset_index()
+date_uber = uber.groupby(['SDATE']).size()
+
+date_uber.plot()
+plt.xticks(rotation=45)
+
+date_uber.index = pd.to_datetime(date_uber.index)
+date_uber['2016-01':'2016-05'].plot()
+date_uber.resample('M').mean()
+date_uber.resample('D').mean()
+
 
 sns.factorplot(x='PURPOSE', y='MILES', data=uber, size=10, kind='bar', hue='CATEGORY")
---
+++ 8. Kaggle/5. StudentAlcohol/Alcohol.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 28 13:25:31 2017
+
+@author: MCR007
+"""
+
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import seaborn as sns 
+import datetime as dt 
