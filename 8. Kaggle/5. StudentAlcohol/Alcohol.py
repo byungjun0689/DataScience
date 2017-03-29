d8bf8f575b22802a06230053c1b70998229d034b
byungjun <byungjun0689@gmail.com>
2017-03-29 13:36:35 +0900

    StudentAlcohol analysis finish but Will make jupyter notebook and Precision Data

--- 8. Kaggle/5. StudentAlcohol/Alcohol.py
+++ 8. Kaggle/5. StudentAlcohol/Alcohol.py
@@ -23,31 +23,27 @@ df2['class'] = 'por'
 df = df1.append(df2)
 
 df['class'].value_counts()
-
-
-
 df['sex'].value_counts()
+df['age'].value_counts()
+df['school'].value_counts()
+
 sns.factorplot('age',data=df, kind='count')
 sns.factorplot('school',kind='count', data=df)
-
-df.groupby(['school','Dalc']).size()
-school_dalc = pd.DataFrame({'cnt':df.groupby(['school','Dalc']).size()}).reset_index()
-
-sumOfGP = sum(school_dalc[school_dalc['school']=='GP']['cnt'])
-sumOfMS = sum(school_dalc[school_dalc['school']=='MS']['cnt'])
-
-def changePercent(data):
-    #sumOfGP = sum(data[data['school']=='GP']['cnt'])
-    #sumOfMS = sum(data[data['school']=='MS']['cnt'])
-    if data[0] == "GP":
-        return(data[2]/sumOfGP)
-    else:
-        return(data[2]/sumOfMS)
-        
-school_dalc['percent'] = school_dalc.apply(changePercent,axis=1)
+sns.factorplot('sex',kind='count', data=df)
+sns.factorplot('class',kind='count', data=df)
+sns.factorplot('famsize',kind='count', data=df)
+sns.factorplot('Pstatus',kind='count', data=df)
+sns.factorplot('Medu',kind='count', data=df)
+sns.factorplot('Fedu',kind='count', data=df)
+sns.factorplot('Mjob',kind='count', data=df)
+sns.factorplot('Fjob',kind='count', data=df)
+sns.factorplot('reason',kind='count', data=df)
+sns.factorplot('guardian',kind='count', data=df)
+sns.factorplot('traveltime',kind='count', data=df)
+sns.factorplot('studytime',kind='count', data=df)
+sns.factorplot('failures',kind='count', data=df)
 
 columns = df.columns
-
 discrete = []
 continuous = []
 for i in columns:
@@ -65,5 +61,58 @@ corr = X.corr()
 sns.heatmap(corr, 
             xticklabels=corr.columns.values,
             yticklabels=corr.columns.values)
+
+
+Rel_Dalc = corr['Dalc']
+Rel_Walc = corr['Walc']
+
+Rel_Dalc[Rel_Dalc>0.1]
+Rel_Walc[Rel_Walc>0.1]
+
+Rel_Col_Dal = Rel_Dalc[Rel_Dalc>0.1].index.tolist()
+Rel_Col_Wal = Rel_Walc[Rel_Walc>0.1].index.tolist()
+
+
+Dal_df = X[Rel_Col_Dal]
+Wal_df = X[Rel_Col_Wal]
+
+Dal_corr = Dal_df.corr()
+Wal_corr = Wal_df.corr()
+
+sns.heatmap(Dal_corr, 
+            xticklabels=Dal_corr.columns.values,
+            yticklabels=Dal_corr.columns.values)
+            
+sns.heatmap(Wal_corr, 
+            xticklabels=Wal_corr.columns.values,
+            yticklabels=Wal_corr.columns.values)
+            
+sns.boxplot(x='goout',y='Walc',data=df)            
+sns.boxplot(x='goout',y='Dalc',data=df)         
+
+
+sns.factorplot('Walc',kind='count',hue='sex',data=df)            
+            
+sns.boxplot(y='absences',data=df)      
+sns.boxplot(x='Dalc',y='absences',data=df)      
+sns.boxplot(x='Walc',y='absences',data=df)                  
             
 sns.pairplot(X)
+
+
+df.groupby(['school','Dalc']).size()
+school_dalc = pd.DataFrame({'cnt':df.groupby(['school','Dalc']).size()}).reset_index()
+
+sumOfGP = sum(school_dalc[school_dalc['school']=='GP']['cnt'])
+sumOfMS = sum(school_dalc[school_dalc['school']=='MS']['cnt'])
+
+def changePercent(data):
+    #sumOfGP = sum(data[data['school']=='GP']['cnt'])
+    #sumOfMS = sum(data[data['school']=='MS']['cnt'])
+    if data[0] == "GP":
+        return(data[2]/sumOfGP)
+    else:
+        return(data[2]/sumOfMS)
+        
+school_dalc['percent'] = school_dalc.apply(changePercent,axis=1)
+
