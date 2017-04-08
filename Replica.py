fe418952f083b65f2cc16bc9c99ffba1b2b239ef
LEEBYUNGJUN\byung <byungjun0689@gmail.com>
2017-04-08 22:58:24 +0900

    Get Rating Data Replica Reviews (End Crawling)  - have to analysis data

--- Replica.py
+++ Replica.py
@@ -12,9 +12,13 @@ import seaborn as sns
 import datetime as dt
 import urllib.request 
 from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+#from selenium import webdriver
+#from selenium.webdriver.common.keys import Keys
+import json
+import requests
+import re
 
+'''
 elem = driver.find_element_by_name("q")
 elem.clear()
 elem.send_keys("pycon")
@@ -26,18 +30,17 @@ assert "No results found." not in driver.page_source
 driver.close()
 
 
-
+'''
 url = 'https://play.google.com/store/apps/details?id=com.venticake.retrica&hl=ko#details-reviews'
 
-driver = webdriver.Chrome(executable_path=r'D:\DataScience\chromedriver.exe')
-driver.get(url)
-#req = urllib.request.Request(url)
-#data = urllib.request.urlopen(req).read().decode('utf-8')
+#driver = webdriver.Chrome(executable_path=r'D:\DataScience\chromedriver.exe')
+#driver.get(url)
+req = urllib.request.Request(url)
+data = urllib.request.urlopen(req).read().decode('utf-8')
 
-data = driver.page_source
+#data = driver.page_source
 bs = BeautifulSoup(data, 'html.parser')
-
-#div_list = bs.find_all('div',class_="details-section-body expandable")
+div_list = bs.find_all('div',class_="details-section-body expandable")
 #for div in div_list:
 #    print(div.find_all('div', class_='multicol-column'))
     
@@ -67,11 +70,11 @@ for div_list in div_root_list:
 #len(content_list[0].find_all('div',class_="single-review"))
 #div_root_list[0].find_all('div',class_="expand-pages-container")
 
-content_list = bs.find_all('div',class_="details-wrapper apps")
-if len(content_list) > 0:
-    content_list[0].find_all('div', class_="expand-page")    
+#content_list = bs.find_all('div',class_="details-wrapper apps")
+#if len(content_list) > 0:
+#    content_list[0].find_all('div', class_="expand-page")    
 
-
+'''
 tmp = content_list[0].find_all('div', class_="expand-page")    
 tmp2 = tmp[9].find_all('div',class_="single-review")
 len(tmp2)
@@ -85,60 +88,154 @@ driver.find_element_by_class_name('expand-button expand-next')
 driver.findElement(By.cssSelector(".alert.alert-success");
 driver.findElement(By.className("expand-button expand-next"));
 
-
-
-
-
-
-
-#### lxml
-import lxml.html
-import requests
-
-url = 'https://play.google.com/store/apps/details?id=com.venticake.retrica&hl=ko#details-reviews'
-
- res = requests.get(url)
- root = lxml.html.fromstring(res.text)
- root[:100]
- tmp = root.cssselect('div.main-content')
- tmp
- tmp[0].text
- 
- 
- 
-    titles = root.cssselect('a.go_naver')
-    for title in titles:
-        article_url = title.attrib['href']
-        art_res = requests.get(article_url)
-        art_root = lxml.html.fromstring(art_res.text)
-        body = art_root.cssselect('#articleBodyContents')[0]
-        articles.append(body.text_content())
-
-
-
+'''
 
 # ajax
-import json
+
+
 url = "https://play.google.com/store/getreviews?authuser=0"
 param = {'reviewType': '0', 
-         'pageNum': '2', 
+         'pageNum': '10000', 
          'id':'com.venticake.retrica',
          'reviewSortOrder':'4',
          'xhr':'1',
          'token':'ZLqR3TmB64y6koyq8uj1tqqiQ4k:14191636750027',
          'hl':'ko'}
 
-r = requests.post(url, data=param)
-r.text[len(r.text)-1000:len(r.text)]
+res = requests.post(url, data=param)
+res.response
+res.json()
+res.status_code # 400이 될떄까지 pageNum 돌리면 될거같다.
 
-for i in range(1,10):
-    param['pageNum'] = i
-    r = requests.post(url, data=param)
-    print(i)
-    print(r.text[len(r.text)-1000:len(r.text)])
+tmp = res.text
+res_json = json.loads(res.text[6:])
+res_json = res_json[0]
+res_json[2]
 
-param
+bs = BeautifulSoup(res_json[2], 'html.parser')
+review_lists = bs.find_all('div',class_="single-review")
+len(review_lists)
+review_lists[0].find('span',class_="review-date").text # review_date
+review_lists[0].find('div',class_="tiny-star star-rating-non-editable-container")['aria-label'] # Review Star
+re.findall(r'\d+', review_lists[0].find('div',class_="tiny-star star-rating-non-editable-container")['aria-label'])[1] # Get Digits from Rates
+#titles = review_lists[0].find('span',class_="review-title").text # title
+review_lists[0].find('span',class_="review-title").text # title
+review_lists[0].find('div',class_="review-body with-review-wrapper").text # review body
+# body에서 title제거, 전체 리뷰 제거 이후 양옆 Trim해주기 
+# text.strip()
+review_lists[0].find('span',class_="author-name").text.strip()  # get Reviewer
+ 
 
-json.loads(r.text)
 
 
+
+
+''' 
+정리.
+우선 전체 데이터는 첫페이지 URL을 통해서 크롤링. 
+리뷰 : ajax통신을 통해 크롤링.
+'''
+import pandas as pd
+import seaborn as sns 
+import matplotlib.pyplot as plt
+import requests
+import urllib.request
+import json
+import requests
+import re
+
+url = 'https://play.google.com/store/apps/details?id=com.venticake.retrica&hl=ko#details-reviews'
+headers = {'User-Agent': 'Mozilla/5.0 (Windows; U; MSIE 9.0; Windows NT 9.0; en-US)'}
+req = urllib.request.Request(url, headers=headers)
+data = urllib.request.urlopen(req).read().decode('utf-8')
+#req = requests.get(url, headers = headers)
+#data = req.text
+
+bs = BeautifulSoup(data, 'html.parser')
+div_list = bs.find_all('div',class_="details-section-body expandable")
+
+div_root_list = bs.find_all('div',class_="details-wrapper apps")
+
+for div_list in div_root_list:
+    rating = div_list.find_all('div',class_="rating-box")
+    if len(rating)>0:
+        total_num = rating[0].find_all('span', class_="reviews-num")[0].text
+        avg = rating[0].find_all("div",class_="score")[0].text
+        score_detail = rating[0].find_all("div",class_="rating-histogram")[0].find_all('div',class_="rating-bar-container")
+        total_number = {}
+        for score in score_detail:
+            label = score.find('span',class_="bar-label").text.strip()
+            label_number = score.find('span',class_="bar-number").text
+            total_number[label] = label_number
+        total_number['avg'] = avg
+        total_number['total'] = total_num
+        
+
+url = "https://play.google.com/store/getreviews?authuser=0"
+id = 'com.venticake.retrica'
+token = 'ZLqR3TmB64y6koyq8uj1tqqiQ4k:14191636750027'
+
+def GetReviews(url,id,token,pages):  
+    param = {'reviewType': '0', 'pageNum': '10000', 'id':'','reviewSortOrder':'4','xhr':'1','token':'','hl':'ko'}
+    param['id'] = id
+    param['token'] = token
+    
+    review_date_all = []
+    review_star_all = []
+    review_user_all = []
+    review_title_all = []
+    review_body_all = []
+    for i in range(1,pages):
+        param['pageNum'] = i
+        res = requests.post(url, data=param)
+        print(" line : {line}, code : {code}".format(line=i,code=res.status_code))
+        if res.status_code == 400 or i == pages-1:
+            review_df = pd.DataFrame({'DATE':review_date_all,'STAR':review_star_all,'TITLE':review_title_all, 'USER':review_user_all,'BODY':review_body_all})
+            columns_list = ['DATE','USER','STAR','TITLE','BODY']
+            return(review_df[columns_list])
+        else:
+            body = res.text[6:]
+            res_json = json.loads(body)
+            bs = BeautifulSoup(res_json[0][2], 'html.parser')
+            review_lists = bs.find_all('div',class_="single-review")
+            for j in range(1,len(review_lists)):
+                review_date = review_lists[j].find('span',class_="review-date").text # review_date
+                review_star = int(re.findall(r'\d+', review_lists[0].find('div',class_="tiny-star star-rating-non-editable-container")['aria-label'])[1]) # Get Digits from Rates
+                review_title = review_lists[j].find('span',class_="review-title").text # title
+                review_body = review_lists[j].find('div',class_="review-body with-review-wrapper").text # review body
+                review_body = str.replace(review_body,review_title,"")
+                review_body = str.replace(review_body,"전체 리뷰","")
+                review_body = review_body.strip()
+                review_user = review_lists[j].find('span',class_="author-name").text.strip()  # get Reviewer
+                review_date_all.append(review_date)
+                review_star_all.append(review_star)
+                review_user_all.append(review_user)
+                review_title_all.append(review_title)
+                review_body_all.append(review_body)
+                
+                
+DF = GetReviews(url,id,token,10000)
+DF['DATE'] = pd.to_datetime(DF['DATE'],format='%Y년 %m월 %d일')
+DF.head()
+DF = DF.sort_values(by='DATE', ascending=True).reindex()   
+
+sns.factorplot('STAR',kind='count',data=DF) # 실질적으로 1을 준 사용자도 많다. 완전히 실망하거나 만족하거나 하는 성향을 보였다.
+
+DF[DF['STAR']==3]
+
+Under_three = DF[DF['STAR']<=3]
+Over_three = DF[DF['STAR']>=3]
+
+
+# 형태소 분석
+from konlpy.tag import Twitter
+from sklearn.feature_extraction.text import CountVectorizer
+
+tagger = Twitter()
+cv = CountVectorizer(tokenizer=tagger.nouns, max_features=50)
+tdf = cv.fit_transform(Under_three['BODY'])
+
+
+           
+
+                
