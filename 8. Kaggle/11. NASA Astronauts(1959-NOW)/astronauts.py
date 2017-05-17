32f79e62b99f82d7e9e565723271dbe5707e4bd9
byungjun <byungjun0689@gmail.com>
2017-05-17 14:05:19 +0900

    NN + Astronauts

---
+++ 8. Kaggle/11. NASA Astronauts(1959-NOW)/astronauts.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import re
+
+df = pd.read_csv("astronauts.csv")
+
+df['Group'] = df['Group'].astype(object)
+sns.boxplot(df['Year'],)
+
+df.describe()
+len(df['Group'].unique())
+
+df.head()
+
+sns.factorplot('Military Rank',kind='count',data= df)
+plt.xticks(rotation=90)
+
+sns.factorplot('Status', kind='count', data=df)
+plt.xticks(rotation=45)
+
+sns.factorplot('Military Branch', kind='count', data=df, size=8)
+plt.xticks(rotation=90)
+
+df2 = df.copy()
+df2['Military Branch'].apply(lambda x:replace(x," (Retired)"))
