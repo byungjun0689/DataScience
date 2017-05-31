8e73a17cbfcd449c32d8cd88ac51d558e1ed9c5f
byungjun <byungjun0689@gmail.com>
2017-06-01 08:00:00 +0900

    add finding Routine

--- 3. Python/Utilites/findStringinVBForm/findStringinVB.py
+++ 3. Python/Utilites/findStringinVBForm/findStringinVB.py
@@ -4,9 +4,9 @@ import sys,getopt
 
 
 def search(dirname,findstr):
-    
+
     dirlist = os.listdir(dirname)
-    for i in dirlist:  
+    for i in dirlist:
         spath = os.path.join(dirname,i)
         if os.path.isfile(spath):
             ext = os.path.splitext(spath)[-1]
@@ -15,7 +15,7 @@ def search(dirname,findstr):
                     #print(path)
                     if findstr in open(spath,'r').read():
                         print(spath)#print(findstr+" 문자열이 "+path+" 에 존재.")
-						
+
                 except:
                     print("에러 : "+ spath)
                     #f findstr in open(path,'r','utf-8').read():
@@ -27,9 +27,9 @@ def search(dirname,findstr):
 
 
 def search2(dirname,findstr):
-    
+
     dirlist = os.listdir(dirname)
-    for i in dirlist:  
+    for i in dirlist:
         spath = os.path.join(dirname,i)
         if os.path.isfile(spath):
             ext = os.path.splitext(spath)[-1]
@@ -48,9 +48,14 @@ def search2(dirname,findstr):
 
 
 if __name__ == "__main__":
-	print(sys.argv[:])
-	if sys.argv[1] == "help":
-		print("인자 1: 시작 폴더  인자 2: 찾고싶은 문자열 현재는 frm파일만 검색가능.")
-		exit()
-	print("검색시장 15초 정도의 시간소요")
-	search(sys.argv[1],sys.argv[2])
+    print(sys.argv[:])
+    if sys.argv[1] == "help":
+        print("인자 1: 시작 폴더  인자 2: 찾고싶은 문자열 현재는 frm파일만 검색가능.")
+        exit()
+    print("검색시장 15초 정도의 시간소요")
+    print(sys.argv[1])
+    routine = sys.argv[2]
+    if len(sys.argv) >= 4:
+        routine = sys.argv[2] + "^" + sys.argv[3]
+    search(sys.argv[1],routine)
+
