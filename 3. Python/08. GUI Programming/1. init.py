f7429f93ef0257c5c78b6f4c814d65dc6c3a7eaa
byungjun <byungjun0689@gmail.com>
2017-06-07 17:58:41 +0900

    GUI Programming (~ change View)

--- 3. Python/08. GUI Programming/1. init.py
+++ 3. Python/08. GUI Programming/1. init.py
@@ -1,12 +1,26 @@
+# Simple enough, just import everything from tkinter.
 from tkinter import *
+
+
+#download and install pillow:
+# http://www.lfd.uci.edu/~gohlke/pythonlibs/#pillow
 from PIL import Image, ImageTk
 
+
+# Here, we are creating our class, Window, and inheriting from the Frame
+# class. Frame is a class from the tkinter module. (see Lib/tkinter/__init__)
 class Window(Frame):
 
-
+    # Define settings upon initialization. Here you can specify
     def __init__(self, master=None):
+
+        # parameters that you want to send through the Frame class.
         Frame.__init__(self, master)
+
+        #reference to the master widget, which is the tk window
         self.master = master
+
+        #with that, we want to then run init_window, which doesn't yet exist
         self.init_window()
 
     #Creation of init_window
@@ -18,28 +32,49 @@ class Window(Frame):
         # allowing the widget to take the full space of the root window
         self.pack(fill=BOTH, expand=1)
 
+        # creating a menu instance
         menu = Menu(self.master)
         self.master.config(menu=menu)
 
+        # create the file object)
         file = Menu(menu)
-        file.add_command(label='Exit', command=self.client_exit)
-        file.add_command(label='Exit2', command=self.client_exit)
 
-        menu.add_cascade(label='File', menu=file)
+        # adds a command to the menu option, calling it exit, and the
+        # command it runs on event is client_exit
+        file.add_command(label="Exit", command=self.client_exit)
 
+        #added "file" to our menu
+        menu.add_cascade(label="File", menu=file)
+
+
+        # create the file object)
         edit = Menu(menu)
 
-        edit.add_command(label='Undo')
-        menu.add_cascade(label='Edit', menu=edit)
+        # adds a command to the menu option, calling it exit, and the
+        # command it runs on event is client_exit
+        edit.add_command(label="Show Img", command=self.showImg)
+        edit.add_command(label="Show Text", command=self.showText)
 
-        # creating a button instance
-        #quitButton = Button(self, text="Quit", command=self.clien)
+        #added "file" to our menu
+        menu.add_cascade(label="Edit", menu=edit)
 
-        # placing the button on my window
-        #quitButton.place(x=0, y=0)
+    def showImg(self):
+        load = Image.open("chat.png")
+        render = ImageTk.PhotoImage(load)
+
+        # labels can be text or images
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=15, y=15)
+
+
+    def showText(self):
+        text = Label(self, text="Hey there good lookin!")
+        text.pack()
+
 
     def client_exit(self):
-    	exit()
+        exit()
 
 if __name__ == '__main__':
 	root = Tk()
