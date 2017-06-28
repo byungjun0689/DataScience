3d2eec8dc9ade00c21bfceb3a76223e72ce93a1e
byungjun <byungjun0689@gmail.com>
2017-06-28 15:28:20 +0900

    1. Modify findvisualbasic files 2. add Matplotlib (~ing) 3. add Kaggle(instacart data)

--- 3. Python/Utilites/findStringinVBForm/findStringinVB.py
+++ 3. Python/Utilites/findStringinVBForm/findStringinVB.py
@@ -1,33 +1,36 @@
 import os
 import time
 import sys,getopt
+import pandas as pd
 
+data_list = []
 
 def search(dirname,findstr):
-
     dirlist = os.listdir(dirname)
     for i in dirlist:
         spath = os.path.join(dirname,i)
         if os.path.isfile(spath):
             ext = os.path.splitext(spath)[-1]
-            if ext == ".frm":
+            if ext.upper() == ".FRM" or ext.upper() == ".BAS" or ext.upper() == ".CLS":
                 try:
                     #print(path)
                     if findstr in open(spath,'r').read():
                         print(spath)#print(findstr+" 문자열이 "+path+" 에 존재.")
-
+                        data_list.append(spath)
                 except:
-                    print("에러 : "+ spath)
+                    pass
+                    #print("에러 : "+ spath)
                     #f findstr in open(path,'r','utf-8').read():
                         #rint(findstr+" 문자열이 "+path+" 에 존재.")
         elif os.path.isdir(spath):
             search(spath,findstr)
         else:
-            print("오잉")
+            #print("오잉")
+            pass
+
 
 
 def search2(dirname,findstr):
-
     dirlist = os.listdir(dirname)
     for i in dirlist:
         spath = os.path.join(dirname,i)
@@ -50,12 +53,16 @@ def search2(dirname,findstr):
 if __name__ == "__main__":
     print(sys.argv[:])
     if sys.argv[1] == "help":
-        print("인자 1: 시작 폴더  인자 2: 찾고싶은 문자열 현재는 frm파일만 검색가능.")
+        print("인자 1: 시작 폴더  인자 2: 찾고싶은 문자열 현재는 frm, bas, cls 만 검색가능.")
         exit()
-    print("검색시장 15초 정도의 시간소요")
+    print("소요시간은 Folder 의 Depth에 따라 다름.")
     print(sys.argv[1])
     routine = sys.argv[2]
     if len(sys.argv) >= 4:
         routine = sys.argv[2] + "^" + sys.argv[3]
     search(sys.argv[1],routine)
+
+    if len(data_list) > 0:
+        filename = routine.replace(".","") + "_list.csv"
+        pd.Series(data_list).to_csv(filename)
 
--- README.md
+++ README.md
@@ -1,18 +1,18 @@
 # Kookmin University Bigdata MBA
  - Lecture Information
  - Study Information
- ### I. 2016 03. Spring Semester 
-   - Bigdata Management 
+ ### I. 2016 03. Spring Semester
+   - Bigdata Management
    - Data Modeling and SQL (ORACLE)
-   - R Programming 
+   - R Programming
    - Statistics
  ###Ⅱ. 2016 09. Fall Semester
    - Data Mining with R
-   - Statistics II with R 
-   - Python Programming (Basic & Machine Learning) 
+   - Statistics II with R
+   - Python Programming (Basic & Machine Learning)
    - Hadoop System (Ecosystem & Hive, HDFS)
  ### Ⅲ. 2017 03. Spring Semester
    - Visualization. (Tableau + GIS Data)
-   - BigData Analysis and strategy 
-   - Unstructured Data Analysis with Python 
-   - Data Science with Real Business 
+   - BigData Analysis and strategy
+   - Unstructured Data Analysis with Python
+   - Data Science with Real Business
